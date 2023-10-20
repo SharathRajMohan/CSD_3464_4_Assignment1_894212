ae3febcf25d36d079e1020bc7e98f13514769fbe
SharathMohan <109592154+wolfgangtechlabs@users.noreply.github.com>
2023-10-20 18:35:02 -0400

    "Initial Structure"

---
+++ src/Point.java
@@ -0,0 +1,26 @@
+public class Point {
+    double x;
+    double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
---
+++ src/Triangle.java
@@ -0,0 +1,35 @@
+public class Triangle {
+    Point a;
+    Point b;
+    Point c;
+
+    public Point getB() {
+        return b;
+    }
+
+    public void setB(Point b) {
+        this.b = b;
+    }
+
+    public Point getC() {
+        return c;
+    }
+
+    public void setC(Point c) {
+        this.c = c;
+    }
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Point getA() {
+        return a;
+    }
+
+    public void setA(Point a) {
+        this.a = a;
+    }
+}
